33fd219c774f37cec26aea8a13c1f4e96d8b5c10
callmegema <sufyl1016@gmail.com>
2024-02-22 13:33:18 +0100

    install prisma
    setup docker-compose for db
    
    install vitest-environment-vprisma

--- spec/check.spec.ts
+++ spec/check.spec.ts
@@ -4,4 +4,29 @@ describe("check", () => {
   it("show env without error", () => {
     console.log(`env: ${process.env.NODE_ENV}`);
   });
+
+  const prisma = vPrisma.client;
+
+  test("Add user", async () => {
+    const createdUser = await prisma.user.create({
+      data: {
+        name: "user1",
+        email: "user1@example.com",
+      },
+    });
+
+    expect(
+      await prisma.user.findFirst({
+        where: {
+          id: createdUser.id,
+        },
+      })
+    ).toStrictEqual(createdUser);
+    expect(await prisma.user.count()).toBe(1);
+  });
+
+  // Each test case is isolated in a transaction and also rolled back, so it is not affected by another test result.
+  test("Count user", async () => {
+    expect(await prisma.user.count()).toBe(0);
+  });
 });
